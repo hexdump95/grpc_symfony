d660b4f62c0478433b6b84a953c1c21fce3c4160
Sergio Villanueva <sergiovillanueva@protonmail.com>
2025-02-23 23:40:24 -0300

    Add grpc_server

---
+++ grpc_server/src/GRPC/Catalog/CatalogService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\GRPC\Catalog;
+
+use GRPC\Catalog\CatalogInterface;
+use GRPC\Catalog\CatalogItemRequest;
+use GRPC\Catalog\CatalogItemResponse;
+use Psr\Log\LoggerInterface;
+use Spiral\RoadRunner\GRPC;
+
+class CatalogService implements CatalogInterface
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function GetItemById(GRPC\ContextInterface $ctx, CatalogItemRequest $in): CatalogItemResponse
+    {
+        $this->logger->info("Get item by id {$in->getId()}");
+
+        if ($in->getId() <= 0) {
+            throw new GRPC\Exception\GRPCException(
+                "ID should be greater than 0",
+                GRPC\StatusCode::FAILED_PRECONDITION
+            );
+        }
+
+        return (new CatalogItemResponse())
+            ->setId($in->getId())
+            ->setName("Shoes")
+            ->setPrice(400)
+            ->setDescription("The best shoes!");
+    }
+}
+
