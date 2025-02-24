560a1d89342450b53007c1007bcb9782c21ba811
Sergio Villanueva <sergiovillanueva@protonmail.com>
2025-02-23 23:41:38 -0300

    Add grpc_client

---
+++ grpc_client/src/Controller/BasketController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\GRPC\Catalog\CatalogClient;
+use GRPC\Catalog\CatalogInterface;
+use GRPC\Catalog\CatalogItemRequest;
+use Spiral\RoadRunner\GRPC\Context;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BasketController extends AbstractController
+{
+    private CatalogClient $catalogClient;
+
+    public function __construct(CatalogClient $catalogClient)
+    {
+        $this->catalogClient = $catalogClient;
+    }
+
+    #[Route('/api/basket/items', name: 'basket', methods: ['POST'])]
+    public function addBasketItem(Request $request): JsonResponse
+    {
+        $request = json_decode($request->getContent(), true);
+        $response = $this->catalogClient->GetItemById(new Context([]), (new CatalogItemRequest())->setId($request['itemId']));
+        return new JsonResponse($response->serializeToJsonString(), Response::HTTP_OK, [], true);
+    }
+}
+
---
+++ grpc_client/src/GRPC/Catalog/CatalogClient.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\GRPC\Catalog;
+
+use Grpc\BaseStub;
+use GRPC\Catalog\CatalogInterface;
+use GRPC\Catalog\CatalogItemRequest;
+use GRPC\Catalog\CatalogItemResponse;
+use Grpc\ChannelCredentials;
+use Spiral\RoadRunner\GRPC;
+
+class CatalogClient extends BaseStub implements CatalogInterface
+{
+
+    public function __construct($hostname)
+    {
+        parent::__construct(
+            $hostname,
+            ['credentials' => ChannelCredentials::createInsecure()],
+        );
+    }
+
+    public function GetItemById(GRPC\ContextInterface $ctx, CatalogItemRequest $in): CatalogItemResponse
+    {
+        [$response] = $this->_simpleRequest(
+            '/' . self::NAME . '/GetItemById',
+            $in,
+            [CatalogItemResponse::class, 'decode'],
+            (array)$ctx->getValue('metadata'),
+            (array)$ctx->getValue('options')
+        )->wait();
+        return $response;
+    }
+
+}
+
